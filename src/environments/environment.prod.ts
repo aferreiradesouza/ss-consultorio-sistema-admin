1eb47b0f0075a58925f07587d6164f5a9dd4679f
Rafael Souza <rsilveiradesouza@gmail.com>
2021-01-16 18:13:33 -0300

    fix: correção de url http para https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  urlBase: 'http://api.ssistema.com.br/api/',
-  urlSignalR: 'http://api.ssistema.com.br/',
+  urlBase: 'https://api.ssistema.com.br/api/',
+  urlSignalR: 'https://api.ssistema.com.br/',
   production: true,
   version: '0.0.1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  urlBase: 'http://api.ssistema.com.br/api/',
-  urlSignalR: 'http://api.ssistema.com.br/',
+  urlBase: 'https://api.ssistema.com.br/api/',
+  urlSignalR: 'https://api.ssistema.com.br/',
   production: false,
   version: '1.0.0'
 };
